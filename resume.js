5648909683c9846b99fc6cea969443bd87795506
Mano S <manokcg@gmail.com>
2022-04-24 21:41:46 +0530

    myResume

---
+++ resume.js
@@ -0,0 +1,54 @@
+let myResume={
+    "Basic Details": {
+      "name": "Mano S",
+      "email": "manokcg@gamil.com",
+      "phone": 9940116905,
+      "degree": "B.E",
+      "location": {
+        "address": "52/33/2 Bharatheeswarar colony 2nd street, Kodambakkam",
+        "postalCode": 600024,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+  
+    "Designation": [
+      {
+        "company": "xxx-xxxx-xxxx-xxxx-xxxx",
+        "position": "Quality Associate",
+        "startDate": "2022-06-05",
+        "summary": "I am the Quality Associate handling with an experience of 3.5 years handling Quality checks on various web-based projects",
+      },
+    ],
+    "Education": [
+      {
+        "institution": "KCG College of Technology",
+        "department": "Mechanical Engineering",
+        "studyType": "fulltime",
+        "batch start year": 2013,
+        "batch end year": 2017,
+        "gpa": 6.3,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginner",
+        "project": [
+          "build my own resume using JSON iterations",
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Playing cricket",
+      },
+    ]
+  }
+  console.log(myResume);
